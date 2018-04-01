a434b574779e59f4142bb6f0ab9888cbbbc79f38
Olibrius11 <v.lemire@gmail>
2018-04-02 01:01:19 +0200

    Implemented first route and controller

---
+++ src/Blog/Controller/HomePageController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of P5 - OPCR Blog package.
+ *
+ * (c) Vincent Lemire <v.lemire@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blog\Controller;
+
+
+class HomePageController
+{
+    public function index()
+    {
+        return file_get_contents("./templates/home.html");
+    }
+
+}
